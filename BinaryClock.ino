89391ab365f72bf4bd18b1598cf86aae9f1d454a
Ophe <opheophe@gmail.com>
2020-09-22 18:02:23 +0200

    Basic binary counting

--- BinaryClock.ino
+++ BinaryClock.ino
@@ -1,3 +1,19 @@
+#define BIT(n,i) (n>>i&1)
+long now = millis();
+int second = now / 1000;
+int minute = second / 60;
+int hour = minute / 60;
+
+int sec_1_digit;
+int sec_2_digit;
+int min_1_digit;
+int min_2_digit;
+int hour_1_digit;
+int hour_2_digit;
+
+
+boolean led_sec = false;
+
 void setup() {
   Serial.begin(115200);
   pinMode(2, OUTPUT);
@@ -8,7 +24,7 @@ void setup() {
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
-    pinMode(A0, OUTPUT);
+  pinMode(A0, OUTPUT);
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
   pinMode(A3, OUTPUT);
@@ -21,62 +37,80 @@ void setup() {
   pinMode(A6, INPUT);
 }
 
+void set_LED(int pin, int value) {
+  if (value == 0) {
+    digitalWrite(pin, LOW);
+  } else {
+    digitalWrite(pin, HIGH);
+  }
+}
+
 void loop() {
+  now = (millis() + 500000) * 10;
 
-  digitalWrite(2, HIGH);
-  delay(100);
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  delay(100);
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);
-  delay(100);
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
-  delay(100);
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  delay(100);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  delay(100);
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  delay(100);
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  delay(100);
-  digitalWrite(9, LOW);
-  digitalWrite(A0, HIGH);
-  delay(100);
-  digitalWrite(A0, LOW);
-  digitalWrite(A1, HIGH);
-  delay(100);
-  digitalWrite(A1, LOW);
-    digitalWrite(A2, HIGH);
-  delay(100);
-  digitalWrite(A2, LOW);
-    digitalWrite(A3, HIGH);
-  delay(100);
-  digitalWrite(A3, LOW);
-    digitalWrite(A4, HIGH);
-  delay(100);
-  digitalWrite(A4, LOW);
-  digitalWrite(A5, HIGH);
-  delay(100);
-  digitalWrite(A5, LOW);
-    digitalWrite(12, HIGH);
-  delay(100);
-  digitalWrite(12, LOW);
-    digitalWrite(10, HIGH);
-  delay(100);
-  digitalWrite(10, LOW);
-      digitalWrite(13, HIGH);
-  delay(100);
-  digitalWrite(13, LOW);
+  if (now / 1000 % 60 == second) {
+    // do nothing
+  } else {
+    second = now / 1000 % 60;
+    minute = now / 60 / 1000 % 60;
+    hour = minute / 60 / 60 / 1000 % 24;
 
-Serial.print(analogRead(A7));
 
-Serial.print("  ");
-Serial.println(analogRead(A6));
+    if (hour < 10) Serial.print("0");
+    Serial.print(hour);
+    Serial.print(":");
+    if (minute < 10) Serial.print("0");
+    Serial.print(minute);
+    Serial.print(":");
+    if (second < 10) Serial.print("0");
+    Serial.print(second);
+
+    Serial.print("  ");
+    Serial.print(analogRead(A7));
+    Serial.print("  ");
+    Serial.print(analogRead(A6));
+
+    Serial.println();
+
+    sec_1_digit = second % 10;
+    sec_2_digit = (second - sec_1_digit) / 10;
+    min_1_digit = minute % 10;
+    min_2_digit = (minute - min_1_digit) / 10;
+    hour_1_digit = hour % 10;
+    hour_2_digit = (hour - hour_1_digit) / 10;
+
+    // sec blink
+    set_LED(13, BIT(sec_1_digit, 0));
+
+
+    // x0-minutes
+    set_LED(5, BIT(min_2_digit, 0));
+    set_LED(4, BIT(min_2_digit, 1));
+    set_LED(3, BIT(min_2_digit, 2));
+    set_LED(2, BIT(min_2_digit, 3));
+
+    // 0x-minutes
+    set_LED(9, BIT(min_1_digit, 0));
+    set_LED(8, BIT(min_1_digit, 1));
+    set_LED(7, BIT(min_1_digit, 2));
+    set_LED(6, BIT(min_1_digit, 3));
+
+    // x0 seconds
+    set_LED(A3, BIT(sec_2_digit, 0));
+    set_LED(A2, BIT(sec_2_digit, 1));
+    set_LED(A1, BIT(sec_2_digit, 2));
+    set_LED(A0, BIT(sec_2_digit, 3));
+
+    // 0x seconds
+    set_LED(A4, BIT(sec_1_digit, 0));
+    set_LED(A5, BIT(sec_1_digit, 1));
+    set_LED(10, BIT(sec_1_digit, 2));
+    set_LED(12, BIT(sec_1_digit, 3));
+
+
+  }
+
+
+
+
 }
